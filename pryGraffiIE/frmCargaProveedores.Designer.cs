011137df3722a46d3c2fc4e847e940cf509041e1
root <mgiraudo@ies21.edu.ar>
2023-09-26 15:46:14 -0300

    26092023

---
+++ pryGraffiIE/frmCargaProveedores.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace pryGraffiIE
+{
+    partial class frmCargaProveedores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDatos = new System.Windows.Forms.Label();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.txtJuzg = new System.Windows.Forms.TextBox();
+            this.txtExpediente = new System.Windows.Forms.TextBox();
+            this.txtEntidad = new System.Windows.Forms.TextBox();
+            this.txtApertura = new System.Windows.Forms.TextBox();
+            this.txtJurisdiccion = new System.Windows.Forms.TextBox();
+            this.Entidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtLiquidador = new System.Windows.Forms.TextBox();
+            this.btnGrabar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblDatos
+            // 
+            this.lblDatos.Location = new System.Drawing.Point(12, 9);
+            this.lblDatos.Name = "lblDatos";
+            this.lblDatos.Size = new System.Drawing.Size(453, 234);
+            this.lblDatos.TabIndex = 0;
+            this.lblDatos.Text = "label1";
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Entidad,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dgvDatos.Location = new System.Drawing.Point(74, 258);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDatos.Size = new System.Drawing.Size(741, 200);
+            this.dgvDatos.TabIndex = 1;
+            this.dgvDatos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDatos_CellContentClick);
+            this.dgvDatos.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDatos_CellDoubleClick);
+            // 
+            // txtJuzg
+            // 
+            this.txtJuzg.Location = new System.Drawing.Point(392, 137);
+            this.txtJuzg.Name = "txtJuzg";
+            this.txtJuzg.Size = new System.Drawing.Size(100, 20);
+            this.txtJuzg.TabIndex = 2;
+            // 
+            // txtExpediente
+            // 
+            this.txtExpediente.Location = new System.Drawing.Point(286, 137);
+            this.txtExpediente.Name = "txtExpediente";
+            this.txtExpediente.Size = new System.Drawing.Size(100, 20);
+            this.txtExpediente.TabIndex = 3;
+            // 
+            // txtEntidad
+            // 
+            this.txtEntidad.Location = new System.Drawing.Point(74, 137);
+            this.txtEntidad.Name = "txtEntidad";
+            this.txtEntidad.Size = new System.Drawing.Size(100, 20);
+            this.txtEntidad.TabIndex = 4;
+            this.txtEntidad.TextChanged += new System.EventHandler(this.txtEntidad_TextChanged);
+            // 
+            // txtApertura
+            // 
+            this.txtApertura.Location = new System.Drawing.Point(180, 137);
+            this.txtApertura.Name = "txtApertura";
+            this.txtApertura.Size = new System.Drawing.Size(100, 20);
+            this.txtApertura.TabIndex = 5;
+            // 
+            // txtJurisdiccion
+            // 
+            this.txtJurisdiccion.Location = new System.Drawing.Point(498, 137);
+            this.txtJurisdiccion.Name = "txtJurisdiccion";
+            this.txtJurisdiccion.Size = new System.Drawing.Size(100, 20);
+            this.txtJurisdiccion.TabIndex = 6;
+            // 
+            // Entidad
+            // 
+            this.Entidad.HeaderText = "Entidad";
+            this.Entidad.Name = "Entidad";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Apertura";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "N* Expediente";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Juzg.";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Jurisdicción";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Dirección";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Liquidador responsable";
+            this.Column7.Name = "Column7";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(604, 137);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(100, 20);
+            this.txtDireccion.TabIndex = 7;
+            // 
+            // txtLiquidador
+            // 
+            this.txtLiquidador.Location = new System.Drawing.Point(715, 137);
+            this.txtLiquidador.Name = "txtLiquidador";
+            this.txtLiquidador.Size = new System.Drawing.Size(100, 20);
+            this.txtLiquidador.TabIndex = 8;
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Location = new System.Drawing.Point(725, 203);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(75, 23);
+            this.btnGrabar.TabIndex = 9;
+            this.btnGrabar.Text = "grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.btnGrabar_Click);
+            // 
+            // frmCargaProveedores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 506);
+            this.Controls.Add(this.btnGrabar);
+            this.Controls.Add(this.txtLiquidador);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtJurisdiccion);
+            this.Controls.Add(this.txtApertura);
+            this.Controls.Add(this.txtEntidad);
+            this.Controls.Add(this.txtExpediente);
+            this.Controls.Add(this.txtJuzg);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.lblDatos);
+            this.Name = "frmCargaProveedores";
+            this.Text = "Carga de Proveedores";
+            this.Load += new System.EventHandler(this.frmCargaProveedores_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDatos;
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Entidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.TextBox txtJuzg;
+        private System.Windows.Forms.TextBox txtExpediente;
+        private System.Windows.Forms.TextBox txtEntidad;
+        private System.Windows.Forms.TextBox txtApertura;
+        private System.Windows.Forms.TextBox txtJurisdiccion;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtLiquidador;
+        private System.Windows.Forms.Button btnGrabar;
+    }
+}
---
+++ pryGraffiIE/frmCargaProveedores.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace pryGraffiIE
+{
+    public partial class frmCargaProveedores : Form
+    {
+        //declaraciòn de variables globales
+        StreamReader objetoLectorArchivo;
+        StreamWriter objetoEscribeArchivo;
+        string rutaArchivo;
+
+        string leerLinea;
+        string[] separadorTexto;
+
+        public frmCargaProveedores()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNivel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNivel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmCargaProveedores_Load(object sender, EventArgs e)
+        {
+            DirectoryInfo informacionCarpetas = new DirectoryInfo(@"..\..");
+
+            rutaArchivo = informacionCarpetas.FullName;
+
+            rutaArchivo += "\\basedatos\\Listado de aseguradores.csv";
+
+            objetoLectorArchivo = new StreamReader(rutaArchivo);
+
+            while (!objetoLectorArchivo.EndOfStream)
+            {
+                leerLinea = objetoLectorArchivo.ReadLine();
+
+                separadorTexto = leerLinea.Split(';');
+
+                //lblDatos.Text+=objetoLectorArchivo.ReadLine();
+                dgvDatos.Rows.Add(separadorTexto);
+            }
+
+            objetoLectorArchivo.Close();
+        }
+
+        private void dgvDatos_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            lblDatos.Text = "hizo doble clic";
+            
+            txtEntidad.Text = dgvDatos.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtApertura.Text = dgvDatos.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtExpediente.Text =   dgvDatos.Rows[e.RowIndex].Cells[3].Value.ToString();
+            txt
+        }
+
+        private void dgvDatos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void txtEntidad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo informacionCarpetas = new DirectoryInfo(@"..\..");
+
+            rutaArchivo = informacionCarpetas.FullName;
+
+            rutaArchivo += "\\basedatos\\Listado de aseguradores.csv";
+
+            objetoEscribeArchivo = new StreamWriter(rutaArchivo, true);
+
+            objetoEscribeArchivo.WriteLine(txtApertura.Text + ";" +
+                txtEntidad.Text + ";" + txtExpediente.Text);
+
+            objetoEscribeArchivo.Close();
+
+            MessageBox.Show("REgistro realizado");
+
+        }
+    }
+}
