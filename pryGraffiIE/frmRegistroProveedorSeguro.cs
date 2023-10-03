05997d525516a184c6a9a33b17841bd94bfb70a0
root <mgiraudo@ies21.edu.ar>
2023-10-03 17:03:50 -0300

    03102023

--- pryGraffiIE/frmRegistroProveedorSeguro.cs
+++ pryGraffiIE/frmRegistroProveedorSeguro.cs
@@ -9,6 +9,10 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Reflection.Emit;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
 
 
 
