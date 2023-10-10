9b0329d88d577818e05bf918428ec9eca1837024
root <mgiraudo@ies21.edu.ar>
2023-10-10 16:39:58 -0300

    10102023

--- pryGraffiIE/frmIniciarSesion.Designer.cs
+++ pryGraffiIE/frmIniciarSesion.Designer.cs
@@ -33,6 +33,11 @@
             this.lblContraseña = new System.Windows.Forms.Label();
             this.txtContraseña = new System.Windows.Forms.TextBox();
             this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboUsers = new System.Windows.Forms.ComboBox();
+            this.btnTraer = new System.Windows.Forms.Button();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblUsuario
@@ -69,12 +74,63 @@
             this.txtUsuario.Size = new System.Drawing.Size(100, 20);
             this.txtUsuario.TabIndex = 3;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(36, 207);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Usuarios";
+            // 
+            // cboUsers
+            // 
+            this.cboUsers.FormattingEnabled = true;
+            this.cboUsers.Location = new System.Drawing.Point(90, 207);
+            this.cboUsers.Name = "cboUsers";
+            this.cboUsers.Size = new System.Drawing.Size(40, 21);
+            this.cboUsers.TabIndex = 7;
+            // 
+            // btnTraer
+            // 
+            this.btnTraer.Location = new System.Drawing.Point(39, 166);
+            this.btnTraer.Name = "btnTraer";
+            this.btnTraer.Size = new System.Drawing.Size(176, 29);
+            this.btnTraer.TabIndex = 6;
+            this.btnTraer.Text = "Traer informaciòn de USERS";
+            this.btnTraer.UseVisualStyleBackColor = true;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(39, 131);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(176, 29);
+            this.btnConectar.TabIndex = 5;
+            this.btnConectar.Text = "Conectar a la Base";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(36, 246);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(10, 13);
+            this.lblResultado.TabIndex = 9;
+            this.lblResultado.Text = "-";
+            // 
             // frmIniciarSesion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DeepPink;
-            this.ClientSize = new System.Drawing.Size(321, 210);
+            this.ClientSize = new System.Drawing.Size(463, 295);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboUsers);
+            this.Controls.Add(this.btnTraer);
+            this.Controls.Add(this.btnConectar);
             this.Controls.Add(this.txtUsuario);
             this.Controls.Add(this.txtContraseña);
             this.Controls.Add(this.lblContraseña);
@@ -82,6 +138,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmIniciarSesion";
             this.Text = "Iniciar Sesión";
+            this.Load += new System.EventHandler(this.frmIniciarSesion_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,5 +150,10 @@
         private System.Windows.Forms.Label lblContraseña;
         private System.Windows.Forms.TextBox txtContraseña;
         private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboUsers;
+        private System.Windows.Forms.Button btnTraer;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Label lblResultado;
     }
 }
--- pryGraffiIE/frmIniciarSesion.cs
+++ pryGraffiIE/frmIniciarSesion.cs
@@ -16,5 +16,15 @@ namespace pryGraffiIE
         {
             InitializeComponent();
         }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmIniciarSesion_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
