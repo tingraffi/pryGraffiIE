5ec4af84537491119aae665bb10963019a12f65e
root <mgiraudo@ies21.edu.ar>
2023-11-21 15:01:57 -0300

    21112023

--- pryGraffiIE/frmInicioSesion.Designer.cs
+++ pryGraffiIE/frmInicioSesion.Designer.cs
@@ -104,6 +104,7 @@
             // 
             // frmInicioSesion
             // 
+            this.AcceptButton = this.btnIniciarSesion;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.HotPink;
@@ -115,7 +116,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnIniciarSesion);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmInicioSesion";
             this.Text = "Iniciar Sesión";
             this.Load += new System.EventHandler(this.frmInicioSesion_Load);
